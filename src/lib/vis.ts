852f34d6cdcb4b84b1749172cfdcad97ee056905
zak <101057981+miloswrath@users.noreply.github.com>
2025-09-02 15:12:04 -0500

    feat: update WADD during score drag

--- src/lib/vis.ts
+++ src/lib/vis.ts
@@ -584,8 +584,21 @@ export class DecisionLayoutChart {
           const delta = event.x - this.dragInfo.startX;
           let newWPos = Math.max(0, Math.min(this.dragInfo.w, this.dragInfo.startWPos + delta));
           this.dragInfo.g.select("rect.cell-pos").attr("width", newWPos - 1);
-          this.dragInfo.g.select("rect.cell-neg").attr("x", COL_GAP / 2 + newWPos + 1).attr("width", this.dragInfo.w - newWPos - 2);
+          this.dragInfo.g
+            .select("rect.cell-neg")
+            .attr("x", COL_GAP / 2 + newWPos + 1)
+            .attr("width", this.dragInfo.w - newWPos - 2);
           this.dragInfo.g.select("rect.score-handle").attr("x", COL_GAP / 2 + newWPos - 2.5);
+
+          const newScore = 2 * (newWPos / this.dragInfo.w) - 1;
+          this.updateScore(this.dragInfo.d.fid, this.dragInfo.d.oid, newScore);
+          if (showWADD) {
+            const waddScores = this.calculateWADDScores();
+            this.gWADD
+              .selectAll<SVGGElement, Option>("g.wadd")
+              .select("text")
+              .text(d => `WADD: ${waddScores[d.id].toFixed(2)}`);
+          }
         })
         .on("end", () => {
           const newWPos = Number(this.dragInfo.g.select("rect.cell-pos").attr("width")) + 1;
